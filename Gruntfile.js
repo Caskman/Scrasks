c35f548ffc3fb1ba2eb7f9e022e35712c59d1e47
Eric Cascketta <caskman@gmail.com>
2017-07-14 08:05:38 -0500

    better grunt stuff now

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-screeps')
     grunt.loadNpmTasks('grunt-babel')
     grunt.loadNpmTasks('grunt-contrib-clean')
+    grunt.loadNpmTasks('grunt-contrib-copy')
+    grunt.loadNpmTasks('grunt-exec')
 
     var credentials = grunt.file.readJSON('private.json')
 
@@ -36,8 +38,27 @@ module.exports = function(grunt) {
         clean: [
             'dist',
         ],
+        copy: {
+            files: {
+                cwd: 'src/test',  // set working folder / root to copy
+                src: '*.js',           // copy all files and subfolders
+                dest: 'dist/test/',    // destination folder
+                expand: true,           // required when using cwd
+            },
+            // require: ['copy'],
+        },
+        exec: {
+            flow: {
+                cmd: 'flow'
+            }
+        },
     })
 
+    grunt.registerTask(
+        'default',
+        'default build task to clean, compile, and then copy the tests over',
+        ['exec','clean', 'babel', 'copy']
+    )
 
     // grunt.registerTask('test', 'test some stuff', () => {
     //     grunt.log.write("whattup\n").ok()
